c56607d95a21a38b7a1aad8bb6c691cc8e2de768
gomer <swistak@tutamail.com>
2024-10-14 16:59:56 +0300

    cooom

--- 7(2).py
+++ 7(2).py
@@ -4,20 +4,14 @@ data=data.split()
 a = int(data[0])
 b = int(data[1])
 c = int(data[2])
-if a>=b>=c or a>=b>=c:
-    a = str(a)
-    data_output= open("output.txt", "w")
-    data_output.write(a)
-    data_output.close()
-elif b>=c>=a or b>=a>=c:
-    b = str(b)
-    data_output= open("output.txt", "w")
-    data_output.write(b)
-    data_output.close()
-elif c>=a>=b or c>=b>=a:
-    c = str(c)
-    data_output= open("output.txt", "w")
-    data_output.write(c)
-    data_output.close()
-    input_data.close()
-    
+if a>=b and a>=c:
+    more= a
+elif b>=a and b>=c:
+    more = b
+elif c>=a and c>=b:
+    more = c
+more = str(more)
+data_output= open("output.txt", "w")
+data_output.write(more)
+data_output.close()
+input_data.close()
