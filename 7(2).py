de482a46f98e6312d45c4ea3459d11fe289fac4a
gomer <swistak@tutamail.com>
2024-10-14 16:35:20 +0300

    commet

--- 7(2).py
+++ 7(2).py
@@ -5,14 +5,17 @@ a = int(data[0])
 b = int(data[1])
 c = int(data[2])
 if a>=b>=c or a>=b>=c:
+    a = str(a)
     data_output= open("output.txt", "w")
     data_output.write(a)
     data_output.close()
 elif b>=c>=a or b>=a>=c:
+    b = str(b)
     data_output= open("output.txt", "w")
     data_output.write(b)
     data_output.close()
 elif c>=a>=b or c>=b>=a:
+    c = str(c)
     data_output= open("output.txt", "w")
     data_output.write(c)
     data_output.close()
